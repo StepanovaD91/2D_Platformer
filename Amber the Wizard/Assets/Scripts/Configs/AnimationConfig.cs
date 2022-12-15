1376b9845d007863b08ea811f464cfbf13f183a1
StepanovaD91 <81827164+StepanovaD91@users.noreply.github.com>
2022-12-15 23:21:00 +0300

    Lession_1

---
+++ Amber the Wizard/Assets/Scripts/Configs/AnimationConfig.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public enum AnimState
+    {
+        Idle = 0,
+        Run = 1,
+        Jump = 2
+    }
+    [CreateAssetMenu(fileName ="SpriteAnimatorCfg", menuName ="Configs / Animation", order = 1)]
+    public class AnimationConfig : ScriptableObject
+    {
+        [Serializable]
+        public class SpriteSequnce
+        {
+            public AnimState Track;
+            public List<Sprite> Sprites = new List<Sprite>();
+        }
+        public List<SpriteSequnce> Sequnces = new List<SpriteSequnce>();
+    }
+}
